97077f7cbe8d27678df747c1ac9b76df9d278c37
amarmanchala <amar.m16@gmail.com>
2020-03-21 19:06:40 -0400

    Get method has been updated in Master Branch

--- WebApplication3/Controllers/ValuesController.cs
+++ WebApplication3/Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace WebApplication3.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/values/5--This comment added in master branch
         public string Get(int id)
         {
             return "value";
